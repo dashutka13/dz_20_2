6d0f29cae7deefa2c83f8582b9733a543c8010ab
dashuta <it.dashutka@gmail.com>
2024-01-31 17:00:45 +0300

    add dz_20_2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, product
-
+from catalog.views import product, catalog, home_page
 
 app_name = CatalogConfig.name
 
-
 urlpatterns = [
-    path('', index),
-    path('product/', product)
+    path('', home_page, name='home_page'),
+    path('product/<int:pk>/', product, name='product'),
+    path('catalog/', catalog, name='catalog')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
-
-def index(request):
-    return render(request, 'catalog/index.html')
+from catalog.models import Product
 
 
-def product(request):
-    return render(request, 'catalog/product.html')
+def catalog(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Каталог'
+    }
+
+    return render(request, 'catalog/catalog.html', context)
+
+
+def home_page(request):
+    return render(request, 'catalog/home_page.html')
+
+
+def product(request, pk):
+    pokemon = Product.objects.get(pk=pk)
+
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': f'Посмотри как прерасен {pokemon.name}'
+    }
+
+    return render(request, 'catalog/product.html', context)
+
